27eac7c26d12df00fbe4d6cdc856e282cbca72b6
Moroni Capcha <raulmoronicapchacadillo@gmail.com>
2026-05-25 20:27:22 -0500

    W03 team activity

--- src/models/projects.js
+++ src/models/projects.js
@@ -4,7 +4,7 @@ async function getAllProjects() {
     try {
         const sql = `
             SELECT p.*, o.name AS organization_name
-            FROM projects p
+            FROM project p
             JOIN organization o ON p.organization_id = o.organization_id
             ORDER BY p.date ASC`;
 
@@ -34,4 +34,42 @@ const getProjectsByOrganizationId = async (organizationId) => {
     return result.rows;
 };
 
-export { getAllProjects, getProjectsByOrganizationId };
+const getUpcomingProjects = async (number_of_projects) => {
+    const query = `
+    SELECT
+      p.project_id,
+      p.title,
+      p.description,
+      p.date,
+      p.location,
+      p.organization_id,
+      o.name AS organization_name
+    FROM project p
+    JOIN organization o ON p.organization_id = o.organization_id
+    WHERE p.date >= CURRENT_DATE
+    ORDER BY p.date ASC
+    LIMIT $1;
+  `;
+    const result = await db.query(query, [number_of_projects]);
+    return result.rows;
+};
+
+const getProjectDetails = async (id) => {
+    const query = `
+    SELECT
+      p.project_id,
+      p.title,
+      p.description,
+      p.date,
+      p.location,
+      p.organization_id,
+      o.name AS organization_name
+    FROM project p
+    JOIN organization o ON p.organization_id = o.organization_id
+    WHERE p.project_id = $1;
+  `;
+    const result = await db.query(query, [id]);
+    return result.rows.length > 0 ? result.rows[0] : null;
+};
+
+export { getAllProjects, getProjectsByOrganizationId, getUpcomingProjects, getProjectDetails };
